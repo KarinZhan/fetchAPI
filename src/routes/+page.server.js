772046525725839b661742069565f1fa5bd4e1d0
= <=>
2024-04-08 14:40:52 +1000

    Initial Commit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const response = await fetch('https://api.squiggle.com.au/?q=teams')
 
 if(!response.ok){
     return {success:false}
